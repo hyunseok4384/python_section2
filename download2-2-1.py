cb1774a6218e1216c398f91a33a107eff63d3448
hyunseok4384 <hyunseok4384@naver.com>
2019-02-03 17:39:52 +0900

    두번째 버전_1.1

--- download2-2-1.py
+++ download2-2-1.py
@@ -2,9 +2,10 @@ import sys
 import io
 import urllib.request as dw
 
+#adsasd
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
-
+print("Hi")
 
 imgUrl="http://blogfiles.naver.net/20120923_166/ahnsuyeon920_134840415691074Fw5_JPEG/63.jpg"
 htmlURL = "http://google.com"
